59ac8fa8f294cd35e9a1baa5ea478386812248d7
Rogerio Pereira <rogeriolima20042000@yahoo.com.br>
2022-11-22 20:41:26 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -103,6 +103,14 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems(){
 		return items;
 	}
+	
+	public Double getTotal() {
+		double som = 0.0;
+		for(OrderItem x : items) {
+			som += x.getSubTotal();
+		}
+		return som;
+	}
 
 	@Override
 	public int hashCode() {
